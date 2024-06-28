cdd0c40a24ed47750f431845b8567b19c234bc6b
Nikolay Yakimov <nyakimov123@gmail.com>
2024-06-29 01:41:28 +0300

    exc 8

---
+++ src/exercise/exc8/Main.java
@@ -0,0 +1,24 @@
+package exercise.exc8;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String[] inputWords=sc.nextLine().toLowerCase().split(" ");
+        Map<String,Integer> wordCount=new LinkedHashMap<>();
+
+        for(String word:inputWords){
+            wordCount.put(word,wordCount.getOrDefault(word,0)+1);
+        }
+
+        for(var word: wordCount.entrySet()){
+            if(word.getValue()%2==1){
+                System.out.print(word.getKey()+", ");
+            }
+        }
+    }
+}
